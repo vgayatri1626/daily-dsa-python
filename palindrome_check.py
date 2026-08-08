ff9364df8c90b2d0c03450c9dbf41eb0c2d52a3a
GAYATRI <vgayatri1626@gmail.com>
2026-08-08 19:14:52 +0530

    DAY 7: palindrome_check

---
+++ palindrome_check.py
@@ -0,0 +1,11 @@
+def is_palindrome(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True    
+text=input("Enter the string: ")
+print("is palindrome: ",is_palindrome(text))    
