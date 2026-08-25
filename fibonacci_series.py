535e4826e96a07c9c68e058d278f10909573420c
GAYATRI <vgayatri1626@gmail.com>
2026-08-25 21:52:43 +0530

    DAY 18: fibonacci_series

---
+++ fibonacci_series.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    a=0
+    b=1
+    for i in range(n):
+        print(a,end="")
+        a,b=b,a+b
+n=int(input("Enter the number of terms: "))
+print("Fibonacci Series: ")
+fibonacci(n)        
